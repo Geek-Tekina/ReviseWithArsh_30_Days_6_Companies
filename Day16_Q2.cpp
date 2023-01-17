528b84b1f775f1cb34c65bf0c27dc7460289a3d7
Geek-Tekina <tpbc1717@gmail.com>
2023-01-17 20:40:04 +0530

    Flipkart Day 16

---
+++ Day16_Q2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums), greater<int>());
+        int sum = accumulate(begin(nums), end(nums), 0);
+        if (sum % k != 0) {
+            return false;
+        }
+        vector<bool> used(size(nums));
+        return backtrack(nums, sum / k, k, 0, 0, used);
+    }
+private:
+    bool backtrack(vector<int>& nums, int target, int k, int bucket, int start, vector<bool>& used) {
+        if (k == 1) {
+            return true;
+        }
+        
+        if (bucket > target || start >= size(nums)) {
+            return false;
+        }
+        
+        if (bucket == target) {
+            return backtrack(nums, target, k - 1, 0, 0, used);
+        }
+        
+        for (int i = start; i < size(nums); ++i) {
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            if (backtrack(nums, target, k, bucket + nums[i], i + 1, used)) {
+                return true;
+            }
+            used[i] = false;
+        }
+        
+        return false;
+    }
+};
